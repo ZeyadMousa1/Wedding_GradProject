a16d4ee0d46f8111777d58e72fd694d95bf25e1f
MostafaDiab <mostafa3diab55@gmail.com>
2025-03-03 01:20:15 +0200

    init the react project

---
+++ client/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+
+    return (
+        <>
+            <a href="">Home</a>
+            <a href="">Items</a>
+            <a href="">Contact</a>
+        </>
+    )
+
+}
+
